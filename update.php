7b4736d2d66edb8805381bb3cd44bd7fa2eb94e3
Abishek Subedi <sabitakiran602@gmail.com>
2022-03-16 18:54:02 +0545

    fetching comment from database

--- update.php
+++ update.php
@@ -15,8 +15,18 @@
 
     if ($conn -> query($sql)) {
         echo "Data Inserted";
-        header("Location: index.php #comments");
+        header("Location: index.php");
     } else {
         echo "Failed.";
     }
-                
+     
+    $sql2 = "SELECT *FROM tbl_01 WHERE Comments;";
+
+    if ($conn -> query($sql2)) {
+        while ($row = $data -> fetch_assoc()) {
+            echo $row['Comments'] . "<br>";
+        }
+        header("Location: index.php");
+    } else {
+        echo "Failed.";
+    }
