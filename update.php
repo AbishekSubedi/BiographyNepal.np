83818a522d6ef243dde48fde9cbd028cb0f8e239
Abishek Subedi <sabitakiran602@gmail.com>
2022-03-18 21:09:39 +0545

    dpmpme

--- update.php
+++ update.php
@@ -1,36 +1,21 @@
 <?php
 
-    $host = "containers-us-west-29.railway.app:7241";
-    $user = "root";
-    $pass = "lXXj6kLXpvDjBdAIvxMp";
-    $db = "railway";
+$host = "containers-us-west-29.railway.app:7241";
+$user = "root";
+$pass = "lXXj6kLXpvDjBdAIvxMp";
+$db = "railway";
 
-    $conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db);
 
-    $name = $_POST['name'];
-    $comments = $_POST['comment'];
+$name = $_POST['name'];
+$comments = $_POST['comment'];
 
-    $sql = "INSERT INTO comments_01 (Name, Comments)
+$sql = "INSERT INTO comments_01 (Name, Comments)
             VALUES('$name', '$comments');";
 
-    if ($conn -> query($sql)) {
-        echo "Data Inserted";
-        header("Location: index.php");
-    } else {
-        echo "Failed.";
-    }
-    
-    function getComments($conn) {
-        $sql2 = "SELECT * FROM comments_01 WHERE Comments;";
-
-        if ($conn -> query($sql2)) {
-            while ($row = $data -> fetch_assoc()) {
-                echo $row['Names'] . "<br>";
-                echo $row['Comments'] . "<br>";
-            }
-            header("Location: index.php");
-            header("Location: #getcomments");
-        } else {
-            echo "Failed.";
-        }
-    }
+if ($conn->query($sql)) {
+    echo "Data Inserted";
+    header("Location: index.php#getcomments");
+} else {
+    echo "Failed.";
+}
